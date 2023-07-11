ed4d50aec7e42c95f6693e8c253b707db2df486a
Kate <katewolshanat@gmail.com>
2023-07-11 15:43:05 +0200

    initial commit

---
+++ src/js/__tests__/healthIndicator.test.js
@@ -0,0 +1,16 @@
+import { getHealthStatus } from '../heathIndicator';
+
+describe('Health Indicator', () => {
+  test('should return "healthy" if health is more than 50', () => {
+    const character = { name: 'Маг', health: 90 };
+    expect(getHealthStatus(character)).toBe('healthy');
+  });
+  test('should return "wounded" if health is between 15 and 50', () => {
+    const character = { name: 'Маг', health: 30 };
+    expect(getHealthStatus(character)).toBe('wounded');
+  });
+  test('should return "critical" if health is less than 15', () => {
+    const character = { name: 'Маг', health: 10 };
+    expect(getHealthStatus(character)).toBe('critical');
+  });
+});
